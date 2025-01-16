3d1f3d97c4f34aea4429b6fb20aa3265ebcfe9e2
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-16 16:54:01 +0100

    Add Header, Footer, Layout, and Auth Components

---
+++ src/components/layout/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { ScrollRestoration, Outlet } from "react-router-dom";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function Layout() {
+  return (
+    <>
+      <ScrollRestoration />
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
