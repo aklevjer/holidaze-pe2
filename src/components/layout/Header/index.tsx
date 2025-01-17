dbcd657c2c5d581a973c3d0875387f2ecb8f298c
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-17 17:40:56 +0100

    Populate Header with components

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import Logo from "@/components/ui/Logo";
+import NavLink from "@/components/ui/NavLink";
+import SearchForm from "@/components/forms/SearchForm";
+import Navbar from "@/components/layout/Header/Navbar";
+import MenuButton from "@/components/layout/Header/MenuButton";
+import MobileMenu from "@/components/layout/Header/MobileMenu";
+
 export default function Header() {
-  return <header>Header</header>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const openMenu = () => setMenuOpen(true);
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <header className="border-b-4 border-teal-900 shadow-elevated md:border-none md:shadow-none">
+      <div className="container flex h-[70px] items-center justify-between">
+        <div className="flex items-center gap-2">
+          <MenuButton menuOpen={menuOpen} onClick={openMenu} />
+          <Logo asDark />
+        </div>
+
+        <SearchForm className="hidden w-full max-w-xs md:flex" />
+        <NavLink asDark to="/login" className="md:hidden">
+          Login
+        </NavLink>
+      </div>
+      <Navbar />
+      <MobileMenu menuOpen={menuOpen} closeMenu={closeMenu} />
+    </header>
+  );
 }
