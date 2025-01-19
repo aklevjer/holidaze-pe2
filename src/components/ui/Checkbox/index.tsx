0ec85a8784040f183960e120b0a6c6d6458ee19f
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-19 23:43:36 +0100

    Add reusable Checkbox component

---
+++ src/components/ui/Checkbox/index.tsx
@@ -0,0 +1,24 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
+interface CheckboxProps {
+  id: string;
+  label: string;
+  register?: UseFormRegisterReturn;
+}
+
+export default function Checkbox({ id, label, register }: CheckboxProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <input
+        type="checkbox"
+        id={id}
+        {...register}
+        className="size-4 flex-shrink-0 accent-teal-700"
+      />
+
+      <label htmlFor={id} className="text-m">
+        {label}
+      </label>
+    </div>
+  );
+}
