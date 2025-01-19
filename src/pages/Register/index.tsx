29ff1a626e3b862fa14c1474ef369b0e3b517909
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-19 23:47:09 +0100

    Update register page with form

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,3 +1,18 @@
+import { RegisterFormData } from "@/schemas/registerSchema";
+import RegisterForm from "@/components/forms/RegisterForm";
+
 export default function Register() {
-  return <h1>Register</h1>;
+  const handleRegister = (registerData: RegisterFormData) => {
+    console.log(registerData);
+  };
+
+  return (
+    <section className="container grid h-full place-items-center pb-20 pt-12">
+      <div className="w-full max-w-lg rounded-md border border-neutral-300 px-4 py-8 shadow-elevated sm:px-12">
+        <h1 className="mb-2 text-2xl font-semibold capitalize">Register</h1>
+        <p className="mb-4">Please register below in order to login.</p>
+        <RegisterForm onSubmit={handleRegister} />
+      </div>
+    </section>
+  );
 }
