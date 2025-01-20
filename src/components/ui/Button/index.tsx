b79f62bd7af856b8a57f7758eb9a21e3b0ae4a1d
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-20 20:46:51 +0100

    Implement loading state in Button component

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
+import { BiLoaderAlt } from "react-icons/bi";
 
 interface ButtonProps {
   variant: "primary" | "secondary";
@@ -7,11 +8,21 @@ interface ButtonProps {
   type?: "button" | "submit";
   path?: string;
   onClick?: () => void;
+  isLoading?: boolean;
   className?: string;
 }
 
-export default function Button({ variant, children, type, path, onClick, className }: ButtonProps) {
-  const base = "rounded-md px-4 py-2 font-medium capitalize text-neutral-100 transition-colors";
+export default function Button({
+  variant,
+  children,
+  type,
+  path,
+  onClick,
+  isLoading,
+  className,
+}: ButtonProps) {
+  const base =
+    "relative rounded-md px-4 py-2 font-medium capitalize text-neutral-100 transition-colors";
 
   const variants = {
     primary: "bg-teal-700 hover:bg-teal-900",
@@ -23,14 +34,16 @@ export default function Button({ variant, children, type, path, onClick, classNa
   if (path) {
     return (
       <Link to={path} className={classes}>
-        {children}
+        {isLoading && <BiLoaderAlt size={24} className="absolute inset-0 m-auto animate-spin" />}
+        <span className={twMerge(isLoading && "invisible")}>{children}</span>
       </Link>
     );
   }
 
   return (
-    <button type={type} onClick={onClick} className={classes}>
-      {children}
+    <button type={type} onClick={onClick} disabled={isLoading} className={classes}>
+      {isLoading && <BiLoaderAlt size={24} className="absolute inset-0 m-auto animate-spin" />}
+      <span className={twMerge(isLoading && "invisible")}>{children}</span>
     </button>
   );
 }
