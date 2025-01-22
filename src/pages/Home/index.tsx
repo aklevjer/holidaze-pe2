e7460018c5e4ea3d20a80494b5156bee7fa9a8e4
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-22 20:04:13 +0100

    Add LatestVenues component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,16 @@
+import { useLatestVenues } from "@/hooks/venues/useLatestVenues";
 import Hero from "@/components/sections/Hero";
+import LatestVenues from "@/components/sections/LatestVenues";
 
 export default function Home() {
-  return <Hero />;
+  const { venues, isLoading, isError } = useLatestVenues();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <Hero />
+      <LatestVenues venues={venues} isError={isError} />
+    </>
+  );
 }
