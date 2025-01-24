bbc36504025369c5c587dce197c74f189fc1f365
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-24 20:35:49 +0100

    Add SearchBar and SortSelect components

---
+++ src/components/ui/SortSelect/index.tsx
@@ -0,0 +1,29 @@
+interface SortSelectProps {
+  onSort: (sortOptions: string) => void;
+}
+
+export default function SortSelect({ onSort }: SortSelectProps) {
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const sortOptions = e.target.value;
+    onSort(sortOptions);
+  };
+  return (
+    <div className="space-y-2 sm:basis-52">
+      <label htmlFor="sort" className="text-m font-medium">
+        Sort by
+      </label>
+
+      <select
+        id="sort"
+        onChange={handleSelectChange}
+        className="w-full rounded-md border-r-8 border-r-transparent px-2 py-2.5 text-m ring-1 ring-slate-500"
+      >
+        <option value="created_desc">Latest Added</option>
+        <option value="price_asc">Price (Low to High)</option>
+        <option value="price_desc">Price (High to Low)</option>
+        <option value="rating_desc">Rating (High to Low)</option>
+        <option value="rating_asc">Rating (Low to High)</option>
+      </select>
+    </div>
+  );
+}
