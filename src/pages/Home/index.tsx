a1483ef08c57e3aac9764e7b5751fe86ce5321a1
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-24 18:19:58 +0100

    Refactor home page with useVenues hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
-import { useLatestVenues } from "@/hooks/venues/useLatestVenues";
+import { useVenues } from "@/hooks/venues/useVenues";
 import Hero from "@/components/sections/Hero";
 import LatestVenues from "@/components/sections/LatestVenues";
 import NewsletterForm from "@/components/forms/NewsletterForm";
 
 export default function Home() {
-  const { venues, isLoading, isError } = useLatestVenues();
+  const { venues, isLoading, isError } = useVenues({
+    sort: "created",
+    sortOrder: "desc",
+    page: 1,
+    limit: 4,
+  });
 
   if (isLoading) return <p>Loading...</p>;
 
