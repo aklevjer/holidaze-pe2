2527b31f80fb9175bd71df966c1d037b3ee19b7a
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-26 23:59:08 +0100

    Update AddVenue page with form and hook

--- src/pages/AddVenue/index.tsx
+++ src/pages/AddVenue/index.tsx
@@ -1,3 +1,18 @@
+import { useCreateVenue } from "@/hooks/venues/useCreateVenue";
+import { VenueFormData } from "@/schemas/venueSchema";
+import VenueForm from "@/components/forms/VenueForm";
+
 export default function AddVenue() {
-  return <h1>Add venue</h1>;
+  const { createVenue, isPending, isError } = useCreateVenue();
+
+  const handleAddVenue = (venueData: VenueFormData) => {
+    createVenue(venueData);
+  };
+
+  return (
+    <section className="container mb-20 mt-12 max-w-2xl space-y-4">
+      <h1 className="text-2xl font-semibold capitalize">Add venue</h1>
+      <VenueForm onSubmit={handleAddVenue} isPending={isPending} isError={isError} />
+    </section>
+  );
 }
