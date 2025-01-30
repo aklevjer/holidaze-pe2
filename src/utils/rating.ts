907a3a3d307d72c876b89bf2c904e6744f91de62
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-30 02:52:15 +0100

    Add utility for calculating rating stars

---
+++ src/utils/rating.ts
@@ -0,0 +1,23 @@
+export const getRatingStars = (rating: number) => {
+  const totalStars = 5;
+  const solidStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
+  const stars = [];
+
+  for (let i = 0; i < solidStars; i++) {
+    stars.push("solid");
+  }
+
+  if (hasHalfStar) {
+    stars.push("half");
+  }
+
+  const remainingStars = totalStars - stars.length;
+
+  for (let i = 0; i < remainingStars; i++) {
+    stars.push("empty");
+  }
+
+  return stars;
+};
