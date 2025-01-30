aa680fd936a0706a31a3a51bdd815a93744cbf01
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-01-30 03:08:22 +0100

    Update venue details with content and components

--- src/components/venues/SingleVenue/index.tsx
+++ src/components/venues/SingleVenue/index.tsx
@@ -1,9 +1,13 @@
 import { Venue } from "@/types/venue";
 import { DEFAULT_VENUE_IMG } from "@/constants/images";
+
 import Gallery from "@/components/venues/Gallery";
+import Rating from "@/components/venues/Rating";
+import Amenities from "@/components/venues/Amenities";
+import Owner from "@/components/venues/Owner";
 
 export default function SingleVenue({ venue }: { venue: Venue }) {
-  const { name, media } = venue;
+  const { name, description, media, price, rating, meta, owner } = venue;
 
   return (
     <>
@@ -16,6 +20,23 @@ export default function SingleVenue({ venue }: { venue: Venue }) {
           className="aspect-3/2 size-full object-cover md:aspect-5/2 md:rounded-md"
         />
       )}
+
+      <div className="grid gap-10 px-4 md:grid-cols-venue md:justify-between md:px-0">
+        <div className="space-y-10">
+          <div>
+            <h1 className="mb-2 text-3xl font-semibold leading-tight">{name}</h1>
+            <Rating rating={rating} />
+            <p className="mb-4">
+              <span className="text-lg font-semibold">${price}</span> /Night
+            </p>
+            <p>{description}</p>
+          </div>
+
+          <Amenities amenities={meta} />
+        </div>
+
+        <div className="space-y-6">{owner && <Owner owner={owner} />}</div>
+      </div>
     </>
   );
 }
