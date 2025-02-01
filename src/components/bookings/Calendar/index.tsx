252f32d2fe39530617d0fa235f5e0208e8ec8883
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-01 09:01:20 +0100

    Add Calendar component

---
+++ src/components/bookings/Calendar/index.tsx
@@ -0,0 +1,60 @@
+import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+import { BiCalendar } from "react-icons/bi";
+import DatePicker from "react-datepicker";
+
+import { Booking } from "@/types/booking";
+import { sortBookingsByDate } from "@/utils/bookings/sortBookings";
+import { formatBookings } from "@/utils/bookings/formatBookings";
+import { isDateAvailable } from "@/utils/bookings/isDateAvailable";
+
+interface CalendarProps {
+  dateFrom: Date | null;
+  dateTo: Date | null;
+  onChange: (dates: [Date | null, Date | null]) => void;
+  bookings: Booking[];
+  error?: { message?: string };
+}
+
+export default function Calendar({ dateFrom, dateTo, onChange, bookings, error }: CalendarProps) {
+  const sortedBookings = useMemo(() => sortBookingsByDate(bookings), [bookings]);
+  const formattedBookings = useMemo(() => formatBookings(sortedBookings), [sortedBookings]);
+
+  return (
+    <div className="space-y-2">
+      <label htmlFor="dates" className="text-m font-medium">
+        Dates
+      </label>
+
+      <div className="relative">
+        <DatePicker
+          id="dates"
+          minDate={new Date()}
+          startDate={dateFrom}
+          endDate={dateTo}
+          onChange={onChange}
+          calendarStartDay={1}
+          dateFormat="MMM dd, yyyy"
+          placeholderText="Select Dates"
+          autoComplete="off"
+          selectsRange
+          isClearable
+          excludeDateIntervals={formattedBookings}
+          filterDate={(date) => isDateAvailable(date, formattedBookings, dateFrom)}
+          wrapperClassName="w-full"
+          className={twMerge(
+            "w-full cursor-pointer rounded-md border border-slate-500 p-2 pl-8 text-m caret-transparent placeholder:text-teal-900 focus:outline-teal-900",
+            error && "border-red-700 focus:outline-red-700",
+          )}
+        />
+        <BiCalendar size={20} className="absolute left-2 top-1/2 -translate-y-1/2" />
+      </div>
+
+      {error && (
+        <p role="alert" className="text-m font-medium text-red-700">
+          {error.message}
+        </p>
+      )}
+    </div>
+  );
+}
