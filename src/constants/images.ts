34d236ab3188d3910ab1bf5299287f7db2e5f9e9
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-02 22:40:01 +0100

    Add banner fallback image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_AVATAR_IMG = "/images/default-avatar.jpg";
+export const DEFAULT_BANNER_IMG = "/images/default-banner.jpg";
 export const DEFAULT_VENUE_IMG = "/images/default-venue.jpg";
