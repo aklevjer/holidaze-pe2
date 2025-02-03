2c4353db106fdd7a2dc8d4ff9fcdeba5377ea875
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-03 22:34:09 +0100

    Add profile tabs to profile page

---
+++ src/components/profile/Tabs/index.tsx
@@ -0,0 +1,40 @@
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { Profile } from "@/types/user";
+import Bookings from "@/components/profile/Bookings";
+
+interface Tabs {
+  profile: Profile;
+  isOwner: boolean;
+}
+
+export default function Tabs({ profile, isOwner }: Tabs) {
+  const { name, venueManager } = profile;
+
+  return (
+    <TabGroup className="space-y-8">
+      <TabList className="flex gap-1 border-b border-slate-500">
+        {isOwner && (
+          <Tab className="-mb-px rounded-t-md border border-slate-500 bg-neutral-50 px-4 py-2 font-medium text-neutral-500 outline-none hover:text-teal-900 data-[selected]:border-b-transparent data-[selected]:bg-white data-[selected]:text-teal-900 data-[focus]:ring-1 data-[focus]:ring-teal-900">
+            Bookings
+          </Tab>
+        )}
+
+        {venueManager && (
+          <Tab className="-mb-px rounded-t-md border border-slate-500 bg-neutral-50 px-4 py-2 font-medium text-neutral-500 outline-none hover:text-teal-900 data-[selected]:border-b-transparent data-[selected]:bg-white data-[selected]:text-teal-900 data-[focus]:ring-1 data-[focus]:ring-teal-900">
+            Venues
+          </Tab>
+        )}
+      </TabList>
+
+      <TabPanels>
+        {isOwner && (
+          <TabPanel>
+            <Bookings profileName={name} />
+          </TabPanel>
+        )}
+
+        {venueManager && <TabPanel>Venues</TabPanel>}
+      </TabPanels>
+    </TabGroup>
+  );
+}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,15 +4,16 @@ import { useProfileByName } from "@/hooks/profile/useProfileByName";
 
 import Alert from "@/components/ui/Alert";
 import Overview from "@/components/profile/Overview";
+import Tabs from "@/components/profile/Tabs";
 
 export default function Profile() {
   const { name } = useParams();
   const user = useAuthStore((state) => state.user);
 
-  const profileName = name || user?.name;
+  const profileName = String(name || user?.name);
   const isOwner = profileName === user?.name;
 
-  const { profile, isLoading, isError } = useProfileByName(String(profileName));
+  const { profile, isLoading, isError } = useProfileByName(profileName);
 
   if (isLoading) return <p>Loading..</p>;
 
@@ -21,7 +22,18 @@ export default function Profile() {
       {isError && (
         <Alert type="error" message="Oops! Failed to load profile. Please try again later." />
       )}
-      {profile && <Overview profile={profile} isOwner={isOwner} />}
+
+      {profile && (
+        <>
+          <Overview profile={profile} isOwner={isOwner} />
+
+          {isOwner || profile.venueManager ? (
+            <Tabs profile={profile} isOwner={isOwner} />
+          ) : (
+            <Alert type="info" message="This user is not a venue manager." />
+          )}
+        </>
+      )}
     </section>
   );
 }
