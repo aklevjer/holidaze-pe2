ffe0d603e2e43a972f891d714c1b36f7c0fc57f4
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-06 02:45:19 +0100

    Add query param to profile venues hook

--- src/hooks/profile/useProfileVenues.ts
+++ src/hooks/profile/useProfileVenues.ts
@@ -5,7 +5,7 @@ import { VenuesResponse } from "@/types/venue";
 export const useProfileVenues = (profileName: string) => {
   const { data, isLoading, isError } = useQuery<VenuesResponse, Error>({
     queryKey: ["profileVenues", profileName],
-    queryFn: () => apiRequest(`/holidaze/profiles/${profileName}/venues`, "GET"),
+    queryFn: () => apiRequest(`/holidaze/profiles/${profileName}/venues?_bookings=true`, "GET"),
   });
 
   return { venues: data?.data || [], isLoading, isError };
