b69cf9c9ca6c559ea3a0e88dcbbe89b4d14756da
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-06 21:39:47 +0100

    Use total price utility in PriceSummary component

--- src/components/venues/PriceSummary/index.tsx
+++ src/components/venues/PriceSummary/index.tsx
@@ -1,4 +1,5 @@
 import { getNightCount } from "@/utils/bookings/getNightCount";
+import { getTotalPrice } from "@/utils/bookings/getTotalPrice";
 
 interface PriceSummaryProps {
   price: number;
@@ -9,7 +10,7 @@ export default function PriceSummary({ price, dates }: PriceSummaryProps) {
   if (!dates[0] || !dates[1]) return null;
 
   const nightCount = getNightCount(dates[0], dates[1]);
-  const totalPrice = Math.round(nightCount * price);
+  const totalPrice = getTotalPrice(dates[0], dates[1], price);
 
   return (
     <p className="flex flex-wrap justify-between gap-2 text-m">
