69c3fdf30984074bf73ce6ccdfa57fa69f694186
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-08 21:39:50 +0100

    Add reusable CardSkeleton component

---
+++ src/components/skeletons/CardSkeleton/index.tsx
@@ -0,0 +1,21 @@
+import Skeleton from "@/components/ui/Skeleton";
+
+export default function CardSkeleton() {
+  return (
+    <div className="grid grid-rows-card overflow-hidden rounded-md border border-neutral-300 shadow-elevated">
+      <Skeleton className="aspect-3/2 rounded-b-none" />
+
+      <div className="flex flex-col justify-between gap-4 p-4">
+        <div className="space-y-1">
+          <Skeleton className="h-6 w-2/3" />
+          <Skeleton className="h-5 w-1/2" />
+        </div>
+
+        <div className="flex justify-between gap-2">
+          <Skeleton className="h-5 w-1/3" />
+          <Skeleton className="h-5 w-1/6" />
+        </div>
+      </div>
+    </div>
+  );
+}
