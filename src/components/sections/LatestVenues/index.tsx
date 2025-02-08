752515156eae917b9b61f3cc9e766d165b9d7947
Aleksander Klevjer <alexz_91_@hotmail.com>
2025-02-08 21:42:09 +0100

    Implement loading skeletons on the home page

--- src/components/sections/LatestVenues/index.tsx
+++ src/components/sections/LatestVenues/index.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import { Venue } from "@/types/venue";
 
-import Alert from "@/components/ui/Alert";
+import CardSkeleton from "@/components/skeletons/CardSkeleton";
 import VenueCard from "@/components/venues/VenueCard";
+import Alert from "@/components/ui/Alert";
 
 interface LatestVenuesProps {
   venues: Venue[];
+  isLoading: boolean;
   isError: boolean;
 }
 
-export default function LatestVenues({ venues, isError }: LatestVenuesProps) {
+export default function LatestVenues({ venues, isLoading, isError }: LatestVenuesProps) {
   return (
     <section className="container my-20">
       <div className="mb-8 flex items-center justify-between border-b border-slate-500 pb-2">
@@ -18,6 +20,14 @@ export default function LatestVenues({ venues, isError }: LatestVenuesProps) {
           View all
         </Link>
       </div>
+
+      {isLoading && (
+        <div className="grid grid-cols-list gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-4">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <CardSkeleton key={index} />
+          ))}
+        </div>
+      )}
 
       {isError && (
         <Alert type="error" message="Oops! Failed to load latest venues. Please try again later." />
